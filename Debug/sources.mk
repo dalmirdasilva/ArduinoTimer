1e70f5202676e584449cdac04c5aa0c9a96772c0
Dalmir da Silva <dalmirdasilva@gmail.com>
2016-10-21 17:06:00 -0200

    sync

--- Debug/sources.mk
+++ Debug/sources.mk
@@ -2,30 +2,30 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-O_SRCS := 
-CPP_SRCS := 
 C_UPPER_SRCS := 
-C_SRCS := 
-S_UPPER_SRCS := 
-S_SRCS := 
-OBJ_SRCS := 
-ASM_SRCS := 
 CXX_SRCS := 
 C++_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
 CC_SRCS := 
-OBJS := 
-C_DEPS := 
-LSS := 
-S_DEPS := 
-CXX_DEPS := 
-S_UPPER_DEPS := 
-C++_DEPS := 
-ASM_DEPS := 
-CC_DEPS := 
-ARCHIVES := 
-CPP_DEPS := 
+ASM_SRCS := 
+C_SRCS := 
+CPP_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
 C_UPPER_DEPS := 
+ARCHIVES := 
+S_DEPS := 
+LSS := 
+C_DEPS := 
+CC_DEPS := 
+C++_DEPS := 
+OBJS := 
+CXX_DEPS := 
+ASM_DEPS := 
 SIZEDUMMY := 
+S_UPPER_DEPS := 
+CPP_DEPS := 
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
